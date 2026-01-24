bef884eab6402e93d896c45eecb22dad41f2ed78
iansudderth <ian.sudderth@gmail.com>
2026-01-24 13:13:01 -0500

    add nvim ai

---
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,107 @@
+return {
+  { "MunifTanjim/nui.nvim", lazy = true },
+  {
+    "yetone/avante.nvim",
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- ⚠️ must add this setting! ! !
+    build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+      or "make",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
+    opts = {
+      provider = "haiku",
+      providers = {
+        deepseek = {
+          __inherited_from = "openai",
+          endpoint = "https://openrouter.ai/api/v1",
+          api_key_name = "OPENROUTER_API_KEY",
+          model = "deepseek/deepseek-v3.2",
+        },
+        haiku = {
+          __inherited_from = "openai",
+          endpoint = "https://openrouter.ai/api/v1",
+          api_key_name = "OPENROUTER_API_KEY",
+          model = "anthropic/claude-haiku-4.5",
+        },
+        sonnet = {
+          __inherited_from = "openai",
+          endpoint = "https://openrouter.ai/api/v1",
+          api_key_name = "OPENROUTER_API_KEY",
+          model = "anthropic/claude-sonnet-4.5",
+        },
+        opus = {
+          __inherited_from = "openai",
+          endpoint = "https://openrouter.ai/api/v1",
+          api_key_name = "OPENROUTER_API_KEY",
+          model = "anthropic/claude-opus-4.5",
+        },
+      },
+      acp_providers = {
+        ["opencode-haiku"] = {
+          command = "opencode",
+          args = { "acp", "--model", "anthropic/claude-haiku-4-5" },
+        },
+        ["opencode-sonnet"] = {
+          command = "opencode",
+          args = { "acp", "--model", "anthropic/claude-sonnet-4-5" },
+        },
+        ["opencode-opus"] = {
+          command = "opencode",
+          args = { "acp", "--model", "anthropic/claude-opus-4-5" },
+        },
+      },
+    },
+    keys = {
+      { "<leader>ae", "<cmd>AvanteEdit<CR>", desc = "Edit Avante" },
+      { "<leader>apr", "<cmd>AvanteSwitchProvider haiku<CR>", desc = "Switch Avante Provider to openrouter" },
+      { "<leader>aph", "<cmd>AvanteSwitchProvider opencode-haiku<CR>", desc = "Switch ACP Provider to opencode-haiku" },
+      {
+        "<leader>aps",
+        "<cmd>AvanteSwitchProvider opencode-sonnet<CR>",
+        desc = "Switch ACP Provider to opencode-sonnet",
+      },
+      { "<leader>apo", "<cmd>AvanteSwitchProvider opencode-opus<CR>", desc = "Switch ACP Provider to opencode-opus" },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "stevearc/dressing.nvim", -- for input provider dressing
+      "folke/snacks.nvim", -- for input provider snacks
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+  { "Kaiser-Yang/blink-cmp-avante" },
+}
